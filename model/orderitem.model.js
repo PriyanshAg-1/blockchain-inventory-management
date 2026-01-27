172de939e7f3b6d15a23e25d76696108a67b11fc
PriyanshAg-1 <2k22.cse.32203@gmail.com>
2026-01-27 13:35:43 +0530

    Add order item  model

---
+++ model/orderitem.model.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const OrderItemSchema = new Schema({
+    order: {
+        type: Schema.Types.ObjectId,
+        ref: 'Order',
+        required: true
+    },
+    product: {
+        type: Schema.Types.ObjectId,
+        ref: 'Product',
+        required: true
+    },
+    quantity: {
+        type: Number,
+        min: 1,
+        required: true
+    }
+});
+
+module.exports = mongoose.model("OrderItem", OrderItemSchema);
